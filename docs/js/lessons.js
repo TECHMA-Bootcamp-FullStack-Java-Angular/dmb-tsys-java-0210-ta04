cdd57c95c02e7fba0f77607a9e9c3ccd91662d02
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-02 19:24:40 +0200

    improved text alignment

--- docs/js/lessons.js
+++ docs/js/lessons.js
@@ -11,13 +11,13 @@ export const lessons = [
         numExercise: 1,
         title: '',
         description:`
-        <p class="m-0">Escribe un programa Java que realice lo siguiente:
-        <p class="m-0"> Declarar una variable N de tipo int, una variable A de tipo double y una variable C de tipo char;
-        <p> Asignar valor a cada una de ellas y continuación, muetra por pantalla:
-        <li> El valor de cada variable;
-        <li> La suma de N + A;
-        <li> La diferencia de A y N;
-        <li> El valor numérico al carácter correspondiente que contiene la variable C.`,
+        <p class="m-0">Escribe un programa Java que realice lo siguiente:</p>
+        <p class="m-0"> Declarar una variable N de tipo int, una variable A de tipo double y una variable C de tipo char </p>
+        <p> Asignar valor a cada una de ellas y continuación, muetra por pantalla:</p>
+        <li> El valor de cada variable </li>
+        <li> La suma de N + A </li>
+        <li> La diferencia de A y N </li>
+        <li> El valor numérico al carácter correspondiente que contiene la variable C </li>`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_01/Main_EJ01.java',
         urlPG: 'https://www.jdoodle.com/embed/v0/6EzG'
         
@@ -33,12 +33,12 @@ export const lessons = [
       {
         numExercise: 3,
         title: '',
-        description: `<p class="m-0"> Escribe un programa Java que realice lo siguiente: <p/>
-        <ul> Declarar dos variables X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una un valor. <p/>
-        A, muestra a continuacion por pantalla:<p/>
-        El valor de cada variable, la suma X+Y, la diferencia XY, el producto X*Y, el cociente X/Y y el resto X%Y.
-        <li>La suma N+M, la diferencia NM, el producto N*M, el cociente N/M, el resto N%M. La suma X*N, el cociente Y/M y resto Y%M.
-        <li>El doble de cada variable, la suma de cada variable y  yl producto de todas las variables;`,
+        description: `<p class="m-0"> Escribe un programa Java que realice lo siguiente: </p>
+        <p class="m-0"> Declarar dos variables X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una un valor. </p>
+        <p class="m-0">A, muestra a continuacion por pantalla:</p>
+        <p class="m-0"> El valor de cada variable, la suma X+Y, la diferencia XY, el producto X*Y, el cociente X/Y y el resto X%Y </p>
+        <li>La suma N+M, la diferencia NM, el producto N*M, el cociente N/M, el resto N%M. La suma X*N, el cociente Y/M y resto Y%M </li>
+        <li>El doble de cada variable, la suma de cada variable y  yl producto de todas las variables </li>`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_03/Main_EJ03.java',
         urlPG: 'https://www.jdoodle.com/embed/v0/6EzN'
       },
@@ -47,9 +47,9 @@ export const lessons = [
         title: '',
         description: `<p class="m-0">Escribe un programa Java que declare una variable entera N y asígnale un valor.
         A escribe a continuacion las instrucciones que realicen los siguientes:
-        <li>Incrementar N en 77;
-        <li>Decrementarla en 3;
-        <li>Duplicar su valor.`,
+        <li>Incrementar N en 77 </li>
+        <li>Decrementarla en 3 </li>
+        <li>Duplicar su valor </li>`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_04/Main_EJ04.java',
         urlPG: 'https://www.jdoodle.com/embed/v0/6EAD'
       },
@@ -58,10 +58,10 @@ export const lessons = [
         title: '',
         description: `Programa Java que declara cuatro variables enteras A, B, C y D y asígnale un valor a cada una. A continuación
         Realice las instrucciones necesarias para que:
-          <li> B tome el valor de C;
-          <li> C tome el valor de A;
-          <li> A tome el valor de D;
-          <li> D tome el valor de B.`,
+          <li> B tome el valor de C </li>
+          <li> C tome el valor de A </li>
+          <li> A tome el valor de D </li>
+          <li> D tome el valor de B </li>`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_05/Main_EJ05.java',
         urlPG: 'https://www.jdoodle.com/embed/v0/6EAG'
       },
