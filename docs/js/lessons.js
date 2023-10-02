658660212def24929888b7ee8af60f1f89238286
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-02 17:01:18 +0200

    links to the exercises are added

--- docs/js/lessons.js
+++ docs/js/lessons.js
@@ -1,8 +1,8 @@
 export const lessons = [
   {
-    numLessons: 1,
+    numLessons: 2,
     issue: 'Java SE',
-    day: 3,
+    day: 5,
     msg: '<i> Completamente entusiamado </i> 🤩',
     myProgress: '🥚',
 
@@ -11,32 +11,58 @@ export const lessons = [
         numExercise: 1,
         title: '',
         description:`
-        <li>Creación de un objeto Integer usando el <b> segundo </b> constructor. </li>
-        <li>Creación de un objeto Integer usando el <b> primer </b> constructor.</li>
-        <li>Obtención del entero que almacena cada objeto Integer.</li>
-        <li>Si no se hace esto, las lineas siguientes causarian error de compilación.</li>`,
-        urlPG: 'https://www.jdoodle.com/iembed/v0/NhU',
-        urlCode: 'https://github.com/d-maza'
+        <p>Escribe un programa Java que realice lo siguiente:
+        <li> declarar una variable N de tipo int; 
+        <li> declarar una variable A de tipo double;
+        <li> declarar una variable C de tipo char;
+        <li> asignar valor a cada una de ellas y continuación, muetra por pantalla:
+        <li> El valor de cada variable;
+        <li> La suma de N + A;
+        <li> La diferencia de A y N;
+        <li> El valor numérico al carácter correspondiente que contiene la variable C.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_01/Main_EJ01.java',
+        urlPG: 'https://www.jdoodle.com/iembed/v0/NhU'
+        
       },
       {
         numExercise: 2,
         title: '',
-        description: 'Genera un comentario en linea //🧐',
-        urlCode: 'https://github.com/d-maza',
+        description: `
+        <p> Declara dos variables numéricas (con el valor que desea), muestra por consola la suma, resto, multiplicación, división y módulo (resto de la división).<p/>`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_02/Main_EJ02.java',
         urlPG: 'https://www.jdoodle.com/iembed/v0/Njb'
       },
       {
         numExercise: 3,
         title: '',
-        description: 'Genera un comentario en varias lineas 📝',
-        urlCode: 'https://github.com/d-maza',
+        description: `<p>Escribe un programa Java que realice lo siguiente: <li>
+        Declarar dos variables X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una un valor. 
+        A, muestra a continuacion por pantalla:
+        <li>El valor de cada variable. La suma X+Y. La diferencia XY. El producto X*Y. El cociente X/Y. El resto X%Y. La suma N+M. La diferencia NM. El producto N*M. El cociente N/M. El resto N%M. La suma X*N. El cociente Y/M. El resto Y%M. El doble de cada variable. La suma de cada variable. El producto de todas las variables;`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_03/Main_EJ03.java',
         urlPG: 'https://www.jdoodle.com/iembed/v0/Njc'
       },
       {
         numExercise: 4,
         title: '',
-        description: 'Genera un Hola Mundo que se vea por consola 👋🏼🌍',
-        urlCode: 'https://github.com/d-maza',
+        description: `Escribe un programa Java que declare una variable entera N y asígnale un valor.
+        A escribe a continuacion las instrucciones que realicen los siguientes:
+        <li>Incrementar N en 77;
+        <li>Decrementarla en 3;
+        <li>Duplicar su valor.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_04/Main_EJ04.java',
+        urlPG: 'https://www.jdoodle.com/iembed/v0/Njd'
+      },
+      {
+        numExercise: 5,
+        title: '',
+        description: `Programa Java que declara cuatro variables enteras A, B, C y D y asígnale un valor a cada una. A continuación
+        Realice las instrucciones necesarias para que:
+          <li> B tome el valor de C;
+          <li> C tome el valor de A;
+          <li> A tome el valor de D;
+          <li> D tome el valor de B.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0210-ta04/blob/main/src/com/tmbs/ta04_05/Main_EJ05.java',
         urlPG: 'https://www.jdoodle.com/iembed/v0/Njd'
       },
     ]
